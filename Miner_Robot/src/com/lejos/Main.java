3053eea6f581bae34f8f78933181087669d57cea
batangpinangga <Alena@Alenas-MacBook-Pro.local>
2015-12-28 16:02:37 +0200

    take BallerGrabber out of the file for this time

--- Miner_Robot/src/com/lejos/Main.java
+++ Miner_Robot/src/com/lejos/Main.java
@@ -1,23 +1,40 @@
 package com.lejos;
 
+import lejos.hardware.Button;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
 import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.hardware.sensor.EV3UltrasonicSensor;
-import lejos.robotics.RegulatedMotor;
 import lejos.robotics.navigation.DifferentialPilot;
 
 public class Main {
 	static int maze_length = 198;
 	EV3UltrasonicSensor ultrasonic_up;
 	EV3UltrasonicSensor ultrasonic_down;
-	DifferentialPilot pilot;
-	RegulatedMotor motor_ultrasonic;
+	EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
+	EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.D);
+	DifferentialPilot pilot = new DifferentialPilot(5.5, 11.73, leftMotor, rightMotor, false);
+
 	EV3GyroSensor gyroSensor;
 	
 	public static void main(String[] args) {
-		
+		buttonInterface();
 	}
 	
-	public void ButtonInterface(){
-		
+	public static void buttonInterface(){
+		while (Button.readButtons() != Button.ID_ESCAPE) {
+		}
+		while (Button.readButtons() != Button.ID_UP){
+			
+		}
+		while (Button.readButtons() != Button.ID_RIGHT){
+			
+		}
+		while (Button.readButtons() != Button.ID_DOWN){
+			
+		}
+		while (Button.readButtons() != Button.ID_UP){
+			
+		}
 	}
 }
